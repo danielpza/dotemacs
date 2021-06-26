039bf6e17b5967b349a8d0365d26bcee55bd335c
Daniel Perez Alvarez <danielpza@protonmail.com>
2021-06-26 00:50:52 -0400

    update

--- modules/format-buffer.el
+++ modules/format-buffer.el
@@ -4,13 +4,13 @@
 
 (provide 'format-buffer)
 
-(defun format-buffer--indent-buffer ()
+(defun format-buffer-indent-buffer()
   "Indent whole buffer with `indent-region' command."
   (interactive)
   (save-excursion
     (indent-region (point-min) (point-max) nil)))
 
-(defvar format-buffer-fn 'format-buffer--indent-buffer)
+(defvar format-buffer-fn 'format-buffer-indent-buffer)
 
 (defun format-buffer()
   "Format whole buffer with `format-buffer-fn' function."
